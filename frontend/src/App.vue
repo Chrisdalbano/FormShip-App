0945dff48ddd91dadf2227d302465137d7785c9e
Chrisdalb <chrisdalbano12@gmail.com>
2024-10-11 13:43:43 -0400

    First commit, init project structure

---
+++ frontend/src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <HomeView />
+    <AboutView />
+  </div>
+</template>
+<script setup>
+import AboutView from './views/AboutView.vue'
+import HomeView from './views/HomeView.vue'
+</script>
---
+++ frontend/src/views/AboutView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="zen-container">
+    <h1>About This Webapp</h1>
+    <p>
+      Our goal is to maintain simplicity and clarity in everything we build.
+    </p>
+  </div>
+</template>
+
+<script setup></script>
