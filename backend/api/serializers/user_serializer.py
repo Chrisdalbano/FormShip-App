9c97c779af04fbcb2d3330ace93564d06757108d
Chrisdalb <chrisdalbano12@gmail.com>
2024-11-05 22:13:36 -0500

    Bug fixed on username sharing set to EMPTY, set to email by default

--- backend/api/serializers/user_serializer.py
+++ backend/api/serializers/user_serializer.py
@@ -22,6 +22,7 @@ class RegisterSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(
             email=validated_data["email"],
+            username=validated_data["email"],
             password=validated_data["password"],
             first_name=validated_data.get("first_name"),
             last_name=validated_data.get("last_name"),
